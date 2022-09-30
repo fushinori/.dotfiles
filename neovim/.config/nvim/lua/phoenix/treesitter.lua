7266bf370785ddda457f75d577935df1e97ed295
Rishi <git@fushinori.me>
2022-09-30 21:02:16 +0530

    [neovim] Set indentation to 2 spaces

--- neovim/.config/nvim/lua/phoenix/treesitter.lua
+++ neovim/.config/nvim/lua/phoenix/treesitter.lua
@@ -2,8 +2,8 @@ local status, treesitter = pcall(require, 'nvim-treesitter.configs')
 if (not status) then return end
 
 treesitter.setup({
-    ensure_installed = { 'python', 'rust', 'css', 'html',
-        'javascript', 'typescript', 'lua' },
-    auto_install = true,
-    highlight = { enable = true },
+  ensure_installed = { 'python', 'rust', 'css', 'html',
+    'javascript', 'typescript', 'lua' },
+  auto_install = true,
+  highlight = { enable = true },
 })
