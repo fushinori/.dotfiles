41316303fd570369ce0d8c75982f5d9a8837a535
Rishi <git@fushinori.me>
2023-11-18 17:40:25 +0530

    Add nvim-cmp capabilities for css-ls

--- neovim/.config/nvim/lua/phoenix/lsp/servers/cssls.lua
+++ neovim/.config/nvim/lua/phoenix/lsp/servers/cssls.lua
@@ -1,11 +1,16 @@
 local lspconfig_status, lspconfig = pcall(require, 'lspconfig')
 if (not lspconfig_status) then return end
+local status, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if (not status) then return end
 
 local on_attach = unpack(require("phoenix.lsp.common"))
 
---Enable (broadcasting) snippet capability for completion
+-- Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- Add additional capabilities supported by nvim-cmp
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 lspconfig.cssls.setup {
 	on_attach = on_attach,
