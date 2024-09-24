3d2d2cfb5b60569133e7a2fb8c60c6b6f24640af
Rishi <git@fushinori.me>
2024-09-24 18:23:19 +0530

    Add tsls to language servers

--- neovim/.config/nvim/lua/phoenix/lsp/lspconfig.lua
+++ neovim/.config/nvim/lua/phoenix/lsp/lspconfig.lua
@@ -1,20 +1,22 @@
-local lspconfig_status, lspconfig = pcall(require, 'lspconfig')
-if (not lspconfig_status) then return end
+local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status then
+  return
+end
 
 local on_attach, capabilities = unpack(require("phoenix.lsp.common"))
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'pyright', 'html', 'emmet_ls', 'tsserver', 'astro', 'tailwindcss', 'svelte', 'cssmodules_ls' }
+local servers = { "pyright", "html", "emmet_ls", "ts_ls", "astro", "tailwindcss", "svelte", "cssmodules_ls" }
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp].setup({
     on_attach = on_attach,
     capabilities = capabilities,
-  }
+  })
 end
 
 -- Common keymaps
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
