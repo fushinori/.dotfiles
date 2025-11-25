0c75da169d83d8459061fabf3e086ba30884e744
Rishi <git@fushinori.dev>
2025-11-25 14:58:50 +0530

    Update telescope keymaps

--- neovim/.config/nvim/lua/phoenix/telescope.lua
+++ neovim/.config/nvim/lua/phoenix/telescope.lua
@@ -1,17 +1,17 @@
-local status = pcall(require, "telescope")
-if not status then
+local ok, telescope = pcall(require, "telescope")
+if not ok then
   return
 end
 
--- Keybindings for telescope
-local keymap = vim.keymap.set
-local opts = { noremap = true }
+local builtin = require("telescope.builtin")
 
-keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
-keymap("n", "<leader>gs", "<cmd>lua require('telescope.builtin').git_status()<cr>", opts)
-keymap("n", "<leader>gc", "<cmd>lua require('telescope.builtin').git_commits()<cr>", opts)
-keymap("n", "<leader>gb", "<cmd>lua require('telescope.builtin').git_branches()<cr>", opts)
-keymap("n", "<leader>dd", "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>", opts)
-keymap("n", "<leader>lr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", opts)
-keymap("n", "<leader>lt", "<cmd>lua require('telescope.builtin').lsp_type_definitions()<cr>", opts)
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Telescope git status" })
+vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Telescope git commits" })
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Telescope git branches" })
+vim.keymap.set("n", "<leader>dd", builtin.lsp_definitions, { desc = "Telescope LSP definitions" })
+vim.keymap.set("n", "<leader>lr", builtin.lsp_references, { desc = "Telescope LSP references" })
+vim.keymap.set("n", "<leader>lt", builtin.lsp_type_definitions, { desc = "Telescope LSP type definitions" })
