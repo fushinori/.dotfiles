7a02ded6eec8ef754d629cdae1c900464d485fe5
Rishi <git@fushinori.dev>
2026-01-12 23:14:17 +0530

    Edit none-ls config
    Only activate eslint and prettier sources if their config files exist in the root directory.

--- neovim/.config/nvim/lua/phoenix/lsp/null_ls.lua
+++ neovim/.config/nvim/lua/phoenix/lsp/null_ls.lua
@@ -3,23 +3,88 @@ if not status then
   return
 end
 
+local utils = require("null-ls.utils")
 local on_attach = unpack(require("phoenix.lsp.common"))
 
+-- Helpers
+local function has_any_file(root, files)
+  if not root then
+    return false
+  end
+
+  for _, file in ipairs(files) do
+    if vim.fn.filereadable(root .. "/" .. file) == 1 then
+      return true
+    end
+  end
+
+  return false
+end
+
+local function has_eslint_config(root)
+  return has_any_file(root, {
+    ".eslintrc",
+    ".eslintrc.js",
+    ".eslintrc.cjs",
+    ".eslintrc.json",
+    ".eslintrc.yaml",
+    ".eslintrc.yml",
+    "eslint.config.js",
+    "eslint.config.mjs",
+    "eslint.config.cjs",
+  })
+end
+
+local function has_prettier_config(root)
+  return has_any_file(root, {
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yaml",
+    ".prettierrc.yml",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    "prettier.config.js",
+    "prettier.config.cjs",
+    "prettier.config.mjs",
+  })
+end
+
 -- Sources
-local eslint_actions = require("none-ls.code_actions.eslint_d").with({
-  extra_filetypes = { "astro" },
-})
-local eslint_diagnostics = require("none-ls.diagnostics.eslint_d").with({
-  extra_filetypes = { "astro" },
-})
-local prettierd = null_ls.builtins.formatting.prettierd.with({
-  extra_filetypes = { "astro" },
-})
+local sources = {}
 
-local stylua = null_ls.builtins.formatting.stylua
+-- Stylua
+table.insert(sources, null_ls.builtins.formatting.stylua)
 
-local sources = { eslint_actions, eslint_diagnostics, prettierd, stylua }
+local root = utils.get_root()
 
+-- Prettier
+if has_prettier_config(root) then
+  table.insert(
+    sources,
+    null_ls.builtins.formatting.prettierd.with({
+      extra_filetypes = { "astro" },
+    })
+  )
+end
+
+-- ESLint
+if has_eslint_config(root) then
+  table.insert(
+    sources,
+    require("none-ls.code_actions.eslint_d").with({
+      extra_filetypes = { "astro" },
+    })
+  )
+
+  table.insert(
+    sources,
+    require("none-ls.diagnostics.eslint_d").with({
+      extra_filetypes = { "astro" },
+    })
+  )
+end
+
+-- Setup
 null_ls.setup({
   on_attach = on_attach,
   sources = sources,
